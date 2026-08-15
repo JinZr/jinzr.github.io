cd5e08ab8177a2bc7f1b3dd18bc4eb66ae08d618
zr_jin <peter.jin.cn@gmail.com>
2026-08-16 01:04:43 +0800

    Use Shanghai timezone for build date

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ const lastEdited = new Date().toLocaleDateString('en-US', {
   month: 'long',
   day: 'numeric',
   year: 'numeric',
+  timeZone: 'Asia/Shanghai',
 });
 
 export default defineConfig({
